b9d576af0bc7a5bdec28e065f056e89c33b292d8
Islam <islam7407@gmail.com>
2017-12-18 20:10:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+import Cookies from 'universal-cookie';
+
+export default function universalCookieMiddleware(socket) {
+    return new Cookies(socket.request.headers.cookie || '', {
+        onSet(name, value, options) {
+            if (!socket.request.cookie || socket.request.headersSent) {
+                return;
+            }
+
+            const expressOpt = { ...options };
+            if (expressOpt.maxAge) {
+                expressOpt.maxAge = options.maxAge * 1000;
+            }
+
+            socket.request.cookie(name, value, expressOpt);
+        },
+        onRemove(name, options) {
+            if (!socket.request.clearCookie || socket.request.headersSent) {
+                return;
+            }
+
+            socket.request.clearCookie(name, options);
+        }
+    });
+}
